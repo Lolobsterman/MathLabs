65bca447c31c2e2797075abbcb391ea45c0f838b
lolobsterman@gmail.com <lolobsterman@gmail.com>
2021-09-29 17:58:33 +0300

    first commit of raw project

---
+++ src/main/java/com/MathLabs/MathLabs/MathLabsApplication.java
@@ -0,0 +1,13 @@
+package com.MathLabs.MathLabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathLabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathLabsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MathLabs/MathLabs/MathLabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MathLabs.MathLabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathLabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
