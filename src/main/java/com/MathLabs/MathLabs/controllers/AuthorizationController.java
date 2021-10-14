41d0c44f5e9425b3666e2e69ed6942bcc4fe0227
lolobsterman@gmail.com <lolobsterman@gmail.com>
2021-10-14 09:17:49 +0300

    pages' design changes, models added

---
+++ src/main/java/com/MathLabs/MathLabs/controllers/AuthorizationController.java
@@ -0,0 +1,13 @@
+package com.MathLabs.MathLabs.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthorizationController {
+
+    @GetMapping("/authorization")
+    public String authorization(Model model) {
+        return "authorization";
+    }
+}
---
+++ src/main/java/models/Post.java
@@ -0,0 +1,7 @@
+package models;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@EntityScan
+public class Post {
+}
